7dd4d2ff1f42e909e18867524f8ab38a85c913bf
Colt Dorsey <cdorsey@xpanxion.com>
2015-06-24 10:26:17 -0500

    My 'first' Java app

---
+++ src/main/java/com/xpanxion/PersonalizedCalculator.java
@@ -0,0 +1,10 @@
+package com.xpanxion;
+
+/**
+ * Created by cdorsey on 6/24/2015.
+ */
+public class PersonalizedCalculator {
+    public static void main(String[] args) {
+        System.out.println("Hello Personalized Calculator");
+    }
+}
---
+++ src/test/java/testing.java
@@ -0,0 +1,10 @@
+/**
+ * Created by cdorsey on 6/24/2015.
+ */
+public class testing
+{
+    public static void Tacos()
+    {
+        System.out.println("hello");
+    }
+}
