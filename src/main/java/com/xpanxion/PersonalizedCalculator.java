9e93acb44bde7ca9d02e54324df3769b47cf7088
Colt Dorsey <cdorsey@xpanxion.com>
2015-06-24 15:11:10 -0500

    Refactoring for single input prompt

--- src/main/java/com/xpanxion/PersonalizedCalculator.java
+++ src/main/java/com/xpanxion/PersonalizedCalculator.java
@@ -7,13 +7,22 @@ import java.util.Scanner;
  */
 public class PersonalizedCalculator {
     public static void main(String[] args) {
-        System.out.println("** Welcome to the Personalized Calculator **");
-        System.out.println("");
         System.out.println("What is your name?");
 
         Scanner keyboard = new Scanner(System.in);
 
         String name = keyboard.next();
-        System.out.println("Hi, " + name);
+        System.out.println("Hello, " + name);
+
+        System.out.println("");
+        System.out.print("Please enter numbers separated by spaces: ");
+        int addend = keyboard.nextInt();
+        int augend = keyboard.nextInt();
+
+        int sum = addend + augend;
+
+        System.out.println("");
+        System.out.print("Results: ");
+        System.out.println("    " + addend + " + " + augend + " = " + sum);
     }
 }
