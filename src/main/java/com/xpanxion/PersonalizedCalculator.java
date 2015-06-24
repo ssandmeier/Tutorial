a497fe5dfb697d11d4a3699cca5868351846c3de
Colt Dorsey <cdorsey@xpanxion.com>
2015-06-24 15:40:06 -0500

    Handling Invalid Operations

--- src/main/java/com/xpanxion/PersonalizedCalculator.java
+++ src/main/java/com/xpanxion/PersonalizedCalculator.java
@@ -7,16 +7,16 @@ import java.util.Scanner;
  */
 public class PersonalizedCalculator {
     public static void main(String[] args) {
-        System.out.println("What is your name?");
+        System.out.print("What is your name? ");
 
         Scanner keyboard = new Scanner(System.in);
 
         String name = keyboard.next();
-        System.out.println("Hello, " + name);
+        System.out.print("Hello, " + name);
 
-        System.out.println("I can perform the following operations:");
+        System.out.println(". I can perform the following operations:");
         System.out.println("1) Addition");
-        System.out.println("What would you like to do?");
+        System.out.print("What would you like to do? ");
         int operation = keyboard.nextInt();
 
         if(operation == 1) {
@@ -28,11 +28,9 @@ public class PersonalizedCalculator {
             int sum = addend + augend;
 
             System.out.println("");
-            System.out.print("Results: ");
-            System.out.println("    " + addend + " + " + augend + " = " + sum);
+            System.out.print("Results: " + addend + " + " + augend + " = " + sum);
         } else {
-            System.out.println("Invalid operation.");
+            System.out.println(String.format("Invalid operation: %s", operation));
         }
-
     }
 }
