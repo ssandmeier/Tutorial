e7a41b30a6a485bb2941eb34af9a8e68564341c7
Colt Dorsey <cdorsey@xpanxion.com>
2015-06-24 10:38:58 -0500

    Added basic personalization

--- src/main/java/com/xpanxion/PersonalizedCalculator.java
+++ src/main/java/com/xpanxion/PersonalizedCalculator.java
@@ -1,10 +1,19 @@
 package com.xpanxion;
 
+import java.util.Scanner;
+
 /**
  * Created by cdorsey on 6/24/2015.
  */
 public class PersonalizedCalculator {
     public static void main(String[] args) {
-        System.out.println("Hello Personalized Calculator");
+        System.out.println("** Welcome to the Personalized Calculator **");
+        System.out.println("");
+        System.out.println("What is your name?");
+
+        Scanner keyboard = new Scanner(System.in);
+
+        String name = keyboard.next();
+        System.out.println("Hi, " + name);
     }
 }
