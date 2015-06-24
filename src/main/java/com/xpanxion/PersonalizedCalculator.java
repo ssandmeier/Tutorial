dbeb42b106e0278a3e62a913657cb418a892f935
Colt Dorsey <cdorsey@xpanxion.com>
2015-06-24 15:52:09 -0500

    Adding more operations

--- src/main/java/com/xpanxion/PersonalizedCalculator.java
+++ src/main/java/com/xpanxion/PersonalizedCalculator.java
@@ -16,7 +16,10 @@ public class PersonalizedCalculator {
 
         System.out.println(". I can perform the following operations:");
         System.out.println("1) Addition");
-        System.out.print("What would you like to do? ");
+        System.out.println("2) Subtraction");
+        System.out.println("3) Multiplication");
+        System.out.println("4) Division");
+        System.out.print("Select an option by numeric value? ");
         int operation = keyboard.nextInt();
 
         if(operation == 1) {
@@ -29,6 +32,37 @@ public class PersonalizedCalculator {
 
             System.out.println("");
             System.out.print("Results: " + addend + " + " + augend + " = " + sum);
+
+        } else if (operation == 2) {
+            System.out.println("");
+            System.out.print("Please enter numbers separated by spaces: ");
+            int minend = keyboard.nextInt();
+            int subend = keyboard.nextInt();
+
+            int difference = minend - subend;
+
+            System.out.println("");
+            System.out.print("Results: " + minend + " - " + subend + " = " + difference);
+        } else if (operation == 3) {
+            System.out.println("");
+            System.out.print("Please enter numbers separated by spaces: ");
+            int multiplier = keyboard.nextInt();
+            int mltiplicand = keyboard.nextInt();
+
+            int product = multiplier * mltiplicand;
+
+            System.out.println("");
+            System.out.print("Results: " + multiplier + " * " + mltiplicand + " = " + product);
+        } else if (operation == 4) {
+            System.out.println("");
+            System.out.print("Please enter numbers separated by spaces: ");
+            int division = keyboard.nextInt();
+            int divisionand = keyboard.nextInt();
+
+            int quotient = division / divisionand;
+
+            System.out.println("");
+            System.out.print("Results: " + division + " / " + divisionand + " = " + quotient);
         } else {
             System.out.println(String.format("Invalid operation: %s", operation));
         }
