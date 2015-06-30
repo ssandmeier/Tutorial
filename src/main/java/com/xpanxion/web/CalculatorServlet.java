4de5cbc1c21bb9d3f28f12302840d40399de62b3
Colt Dorsey <cdorsey@xpanxion.com>
2015-06-30 13:01:15 -0500

    Refactored servlet to use jsp instead

--- src/main/java/com/xpanxion/web/CalculatorServlet.java
+++ src/main/java/com/xpanxion/web/CalculatorServlet.java
@@ -13,21 +13,6 @@ public class CalculatorServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("guest");
-
-        resp.getWriter().write("<html>");
-        resp.getWriter().write("<head>");
-        resp.getWriter().write("<title>Personalized Calculator</title>");
-        resp.getWriter().write("</head>");
-
-        resp.getWriter().write("<body>");
-        resp.getWriter().write("<h1>Hi " + name + "</h1>");
-        resp.getWriter().write("</body>");
-
-        resp.getWriter().write("</html>");
-
-        //super.doPost(req, resp);
+        req.getRequestDispatcher("calculator.jsp").forward(req, resp);
     }
-
-
 }
---
+++ src/main/webapp/calculator.jsp
@@ -0,0 +1,11 @@
+<% String name = request.getParameter("guest"); %>
+<html>
+    <head>
+        <title><%= name %>&apos;s Personalized Calculator</title>
+    </head>
+
+    <body>
+        <h1>Hi, <%= name %></h1>
+    </body>
+</html>
+.
