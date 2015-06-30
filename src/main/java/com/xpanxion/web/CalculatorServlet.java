3a30a9cbfd85e85f5a1c8c4f64219cf84dfe8b9a
Colt Dorsey <cdorsey@xpanxion.com>
2015-06-30 14:23:18 -0500

    Added JSTL and calculator function ability

--- src/main/java/com/xpanxion/web/CalculatorServlet.java
+++ src/main/java/com/xpanxion/web/CalculatorServlet.java
@@ -1,10 +1,15 @@
 package com.xpanxion.web;
 
+import com.xpanxion.Calculation;
+import com.xpanxion.Calculator;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by cdorsey on 6/30/2015.
@@ -13,6 +18,24 @@ public class CalculatorServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String guestName = req.getParameter("guest");
+        session.setAttribute("guest", guestName);
+        req.getRequestDispatcher("calculator.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String operands = req.getParameter("operands");
+        Scanner keyboard = new Scanner(operands);
+
+        HttpSession session = req.getSession();
+        String guestName = (String)session.getAttribute("guest");
+
+        Calculator calculator = new Calculator();
+        Calculation results = calculator.Calculator(Calculator.ADDITION, guestName, keyboard);
+
+        req.setAttribute("resultText", results.getTextResult());
         req.getRequestDispatcher("calculator.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/calculator.jsp
+++ src/main/webapp/calculator.jsp
@@ -1,11 +1,23 @@
-<% String name = request.getParameter("guest"); %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<c:set var="name" value="${sessionScope.guest}" />
 <html>
     <head>
-        <title><%= name %>&apos;s Personalized Calculator</title>
+        <title>${name}&apos;s Personalized Calculator</title>
     </head>
 
     <body>
-        <h1>Hi, <%= name %></h1>
+        <h1>Hi, ${name}</h1>
+
+        <form method="get" action="calculator">
+            <div>
+                <label for="operands">Please enter some numbers separated by spaces</label>
+                <input name="operands" type="text"></input>
+            </div>
+            <div>
+                <input name="calculate" type="submit" value="Calculate"></input>
+            </div>
+        </form>
+        <div>${requestScope.resultText}</div>
+        <a href="logout">Logout</a>
     </body>
 </html>
-.
