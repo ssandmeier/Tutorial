cfaa453413ecf13f6d6d16e854781041b3d08832
Colt Dorsey <cdorsey@xpanxion.com>
2015-07-01 14:54:20 -0500

    Created setup and teardown methods as well as refactored logic into instance methods / variable

--- src/test/java/com/xpanxion/automation/AutomatedTest.java
+++ src/test/java/com/xpanxion/automation/AutomatedTest.java
@@ -13,10 +13,32 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  * Created by cdorsey on 7/1/2015.
  */
 public class AutomatedTest {
-    public static void main(String[] arts) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\cdorsey\\Desktop\\SeaShark34-master\\SeaShark34\\chromedriver.exe");
-        final WebDriver driver = new ChromeDriver();
 
+    private WebDriver driver;
+
+    public static void main(String[] arts) {
+
+        AutomatedTest test = new AutomatedTest();
+
+        test.setup();
+
+        test.successfulLoginLogout();
+        test.missingRequiredField();
+
+        test.tearDown();
+    }
+
+    private void tearDown() {
+        driver.quit();
+    }
+
+    private void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\cdorsey\\Desktop\\SeaShark34-master\\SeaShark34\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+    }
+
+    protected void successfulLoginLogout() {
         driver.get("http://localhost:8080");
         String pageTitle = driver.getTitle();
         System.out.println(pageTitle);
@@ -34,9 +56,21 @@ public class AutomatedTest {
         Wait<WebDriver> wait = new WebDriverWait(driver, 30);
         wait.until(pageIsLoaded);
 
-        WebElement greeting = driver.findElement(By.tagName("h1"));
+        WebElement greeting = driver.findElement(By.tagName("h1")); //By.xpath("/html/body/h1")
         System.out.println(greeting.getText());
 
-        driver.quit();
+        WebElement logout = driver.findElement(By.tagName("a")); //By.linkText("Logout"));
+        logout.click();
+
+        String currentURL = driver.getCurrentUrl();
+        System.out.println(currentURL);
+    }
+
+    protected void missingRequiredField() {
+        driver.get("http://localhost:8080/index.html");
+        driver.findElement(By.name("guest")).submit();
+        WebElement errorMessage = driver.findElement(By.className("error-message"));
+
+        System.out.println(errorMessage.isDisplayed());
     }
 }
