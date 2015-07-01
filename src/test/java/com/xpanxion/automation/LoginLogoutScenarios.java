7d9c0edc4786aca07e3adb584ce1854d62ec1bc6
Colt Dorsey <cdorsey@xpanxion.com>
2015-07-01 16:28:08 -0500

    Refactored console validation to assertions

--- src/test/java/com/xpanxion/automation/LoginLogoutScenarios.java
+++ src/test/java/com/xpanxion/automation/LoginLogoutScenarios.java
@@ -1,5 +1,8 @@
 package com.xpanxion.automation;
 
+
+
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -9,39 +12,37 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by cdorsey on 7/1/2015.
  */
-public class AutomatedTest {
+public class LoginLogoutScenarios {
 
     private WebDriver driver;
 
-    public static void main(String[] arts) {
 
-        AutomatedTest test = new AutomatedTest();
-
-        test.setup();
-
-        test.successfulLoginLogout();
-        test.missingRequiredField();
-
-        test.tearDown();
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\cdorsey\\Desktop\\SeaShark34-master\\SeaShark34\\chromedriver.exe");
+        driver = new ChromeDriver();
     }
 
-    private void tearDown() {
+    @After
+    public void tearDown() {
         driver.quit();
     }
 
-    private void setup() {
+    @Test
+    public void successfulLoginLogout() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\cdorsey\\Desktop\\SeaShark34-master\\SeaShark34\\chromedriver.exe");
+
         driver = new ChromeDriver();
-
-    }
-
-    protected void successfulLoginLogout() {
         driver.get("http://localhost:8080");
         String pageTitle = driver.getTitle();
-        System.out.println(pageTitle);
+
+        assertEquals("Web-based Personalized Calculator", pageTitle);
+
 
         WebElement guestField = driver.findElement(By.name("guest"));
         guestField.sendKeys("Colt");
@@ -57,20 +58,22 @@ public class AutomatedTest {
         wait.until(pageIsLoaded);
 
         WebElement greeting = driver.findElement(By.tagName("h1")); //By.xpath("/html/body/h1")
-        System.out.println(greeting.getText());
+        assertEquals("Hi, Colt", greeting.getText());
+
 
         WebElement logout = driver.findElement(By.tagName("a")); //By.linkText("Logout"));
         logout.click();
 
         String currentURL = driver.getCurrentUrl();
-        System.out.println(currentURL);
+        assertEquals("http://localhost:8080/index.html", currentURL);
     }
 
-    protected void missingRequiredField() {
+    @Test
+    public void missingRequiredField() {
         driver.get("http://localhost:8080/index.html");
         driver.findElement(By.name("guest")).submit();
         WebElement errorMessage = driver.findElement(By.className("error-message"));
 
-        System.out.println(errorMessage.isDisplayed());
+        assertEquals(true, errorMessage.isDisplayed());
     }
 }
